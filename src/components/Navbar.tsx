e083a696aa14b07eb6330686558d17e889440fde
Shawn Barbel <70069553+anthonycoded@users.noreply.github.com>
2025-07-25 21:18:11 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -93,7 +93,7 @@ const Navbar = () => {
       <div className="flex items-center justify-between max-w-7xl mx-auto">
         <div className="flex items-center gap-4 md:gap-8">
           <Link to="/" className="text-red-600 text-2xl md:text-3xl font-bold">
-            CINEPLAY
+            FinesseFlix
           </Link>
           <MobileMenu />
           <div className="hidden md:flex items-center gap-6">
